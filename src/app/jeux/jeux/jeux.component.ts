1a53b4ba1638804d40aedfe165c08f45021182be
MarielleBouchoux <marielle.bouchoux@gmail.com>
2022-01-26 11:44:18 +0100

    template

--- src/app/jeux/jeux/jeux.component.ts
+++ src/app/jeux/jeux/jeux.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Subscription } from 'rxjs';
+import { Jeux } from 'src/app/entity/jeux';
 
 @Component({
   selector: 'app-jeux',
@@ -9,6 +10,7 @@ import { Subscription } from 'rxjs';
 })
 export class JeuxComponent implements OnInit {
 
+  // jeux : Jeux[];
    // initialise le thème du user
    theme: string = "";
    subscription = new Subscription;
