93dfe4c8b09839d34e07ddad01ffb862456ef417
RudLeTerrible <rudolph.nativel@gmail.com>
2022-01-27 13:44:20 +0100

    create class genre,classification,modele-economique,editeur,plateforme

---
+++ src/app/entity/modele-economique.spec.ts
@@ -0,0 +1,7 @@
+import { ModeleEconomique } from './modele-economique';
+
+describe('ModeleEconomique', () => {
+  it('should create an instance', () => {
+    expect(new ModeleEconomique()).toBeTruthy();
+  });
+});
