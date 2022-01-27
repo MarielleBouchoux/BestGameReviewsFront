2f71326c43581758f126c11fdd1fc7600616eac0
RudLeTerrible <rudolph.nativel@gmail.com>
2022-01-27 11:18:02 +0100

    fix page i liste-jeux

--- src/app/jeux/jeux.component.ts
+++ src/app/jeux/jeux.component.ts
@@ -1,30 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { JeuxService } from '../services/jeux.service';
 import { DataService } from '../services/data.service';
 import { Subscription } from 'rxjs';
 import { Jeux } from '../entity/jeux';
-import { dateInputsHaveChanged } from '@angular/material/datepicker/datepicker-input-base';
+import {MatPaginator, MatPaginatorIntl, PageEvent} from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-jeux',
   templateUrl: './jeux.component.html',
   styleUrls: ['./jeux.component.scss']
 })
-export class JeuxComponent implements OnInit {
+export class JeuxComponent implements OnInit  ,AfterViewInit  {
+  pageEvent: PageEvent = new PageEvent;
+  pageIndex:number = 0;
+  pageSize:number = 5;
+  lowValue:number = 0;
+  highValue:number = 5;
 
   // initialise le thème du user
   theme: string = "";
   subscription = new Subscription;
 
+  //Avis pour liste jeux
   private jeu : Jeux= new Jeux(0,"Minecraft", "Truc", new Date("2019-05-27"), "C'est un chouette jeu", "FPS", "https://material.angular.io/assets/img/examples/shiba2.jpg","PEGI 16", "PS5", "play to play");
+  jeux : Jeux[] = [];
+  ELEMENT_DATA: Jeux[] = this.jeux;
+  dataSource = new MatTableDataSource<Jeux>(this.jeux);
 
-  jeux : Jeux[];
 
-  constructor( private data: DataService,private jeuxService: JeuxService) {
+
+  constructor( private data: DataService,private jeuxService: JeuxService,private cdr: ChangeDetectorRef) {
     // onInit on récupère le currentThème du data service
     this.subscription = this.data.currentTheme.subscribe(theme => this.theme = theme);
     this.jeux = []
    }
+   @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(new MatPaginatorIntl,this.cdr);
+
+   ngAfterViewInit() {
+     this.dataSource.paginator = this.paginator;
+     this.paginator.pageSize = 5;
+   }
+
+   getPaginatorData(event :PageEvent){
+     console.log(event);
+     if(event.pageIndex === this.pageIndex + 1){
+        this.lowValue = this.lowValue + this.pageSize;
+        this.highValue =  this.highValue + this.pageSize;
+       }
+    else if(event.pageIndex === this.pageIndex - 1){
+       this.lowValue = this.lowValue - this.pageSize;
+       this.highValue =  this.highValue - this.pageSize;
+      }
+       this.pageIndex = event.pageIndex;
+       this.pageEvent = event;
+       return event;
+ }
 
   ngOnInit(): void {
     this.getAllJeux();
@@ -59,8 +90,13 @@ export class JeuxComponent implements OnInit {
         alert(`Une erreur s'est produite ${error}`)
       }
   });
+  this.pageSize = this.jeux.length;
+    this.pageIndex = 0;
+    this.highValue = 5;
+    this.lowValue = 0;
   console.log(this.jeux);
   return this.jeux;
 
 }
+
 }
