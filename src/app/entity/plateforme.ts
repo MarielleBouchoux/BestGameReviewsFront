c6207ec0227a42cc9088423fb3770ec7612e2b9d
RudLeTerrible <rudolph.nativel@gmail.com>
2022-01-27 13:44:20 +0100

    create class genre,classification,modele-economique,editeur,plateforme

---
+++ src/app/entity/plateforme.ts
@@ -0,0 +1,24 @@
+export class Plateforme {
+  constructor(
+    private _id:number,
+    private _nom:string
+       ){};
+
+
+       public get id() : number {
+         return this._id;
+       }
+
+       public set id(v : number) {
+         this._id = v;
+       }
+
+
+       public get nom() : string {
+         return this._nom;
+       }
+
+       public set nom(v : string) {
+         this._nom = v;
+       }
+}
